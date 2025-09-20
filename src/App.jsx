23ac0a5422b467425c1adc2e814a3d47d954284c
Khawaja Shadan Shakil <shadanshakil9@gmail.com>
2025-09-20 23:44:04 +0500

    app.jsx error resolved

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Header from "./components/Header"
 import Footer from './components/footer'
 import LandingPage from './pages/LandingPage'
 import Services from './pages/Services'
-import Projects from './pages/projects'
+import Projects from './pages/Projects'
 import About from './pages/About'
 import Contact from './pages/Contact'
 
